65f764dcbbd3b767a775cba7733e8ec33792e2ac
DevAdvait <advait25tumbre@gmail.com>
2023-10-04 20:26:46 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const MONGODB_URI =
   "mongodb+srv://boogeymanrt:iA7SaCXWsBykjU5s@cluster0.ycgltkb.mongodb.net/?retryWrites=true&w=majority"; // Replace with your MongoDB URI
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.log(err));
 
 // Create a POST endpoint for submitting data
 app.post("/submitFormData", async (req, res) => {
